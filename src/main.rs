2e3c8e6a9f1cee51fd189d61af7cd8339f323c04
Tim Magoun <timmagoun@gmail.com>
2022-05-25 17:05:09 -0400

    Path expansion and directory reading

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,69 @@
-use std::env;
-use std::fs;
+use clap::{Arg, Command};
+use std::fs::{self, DirEntry};
+use std::path::Path;
 
 fn main() {
-    println!("Rust ls");
-    // Read the flags and the target directory
-    let args: Vec<String> = env::args().collect();
-    let arg_len = args.len();
+    // Set up the command line parser
+    let matches = Command::new("Rust ls")
+        .version("0.0.1")
+        .author("Tim Magoun")
+        .about("Lists files in a directory")
+        .arg(
+            Arg::new("all")
+                .short('a')
+                .takes_value(false)
+                .help("List all files in the current directory, including hidden files]"),
+        )
+        .arg(
+            Arg::new("Trailing")
+                .short('F')
+                .takes_value(false)
+                .help("List files with trailing / added to directory names"),
+        )
+        .arg(
+            Arg::new("long")
+                .short('l')
+                .takes_value(false)
+                .help("Lists in the long format"),
+        )
+        .arg(
+            Arg::new("sort")
+                .short('S')
+                .takes_value(false)
+                .help("Sorts the list by size (descending)"),
+        )
+        .arg(
+            Arg::new("dir")
+                .value_name("DIR")
+                .default_value(".")
+                .help("The directory to list"),
+        )
+        .get_matches();
 
-    // Find the directory to perform the list on
-    let dir_str = &args[arg_len-1];
+    // Set up the directory path expansion
+    let dir_name = matches.value_of("dir").unwrap_or("");
+    println!("Dir name: {:?}", dir_name);
+    let expanded_dir_name = shellexpand::tilde(dir_name).to_string();
+    let expanded_dir_path = Path::new(&expanded_dir_name);
 
-    // Find collection of files in the directory
-    // Make sure the input is valid
-    let dir_md = fs::metadata(dir_str);
-    if dir_md.is_err() || !dir_md.unwrap().is_dir(){
-        println!("Input {} is not a directory", &dir_str);
+    // Vector of DirEntry type that stores all files in the directory
+    let mut file_vec = Vec::<DirEntry>::new();
+    if expanded_dir_path.is_dir() {
+        for entry in fs::read_dir(expanded_dir_path).unwrap() {
+            let entry = entry.unwrap();
+            file_vec.push(entry);
+        }
+    } else {
+        println!("Argument given is not a directory!");
         return;
     }
 
-    let paths = fs::read_dir(dir_str).unwrap();
-
-    for path in paths{
-        println!("Name: {}", path.unwrap().path().display());
+    // Iterate through the files to print out their names
+    for f in &file_vec {
+        let entry_path = f.path();
+        if entry_path.is_dir() {
+            print! {"DIR: "}
+        }
+        println!("{:?}", entry_path.file_name());
     }
-  print!("End");  
-
 }
